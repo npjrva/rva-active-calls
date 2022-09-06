03efe5561a8a1e0db89ed36a050fce93e98699e4
Nick Johnson <johnsoni@deshawresearch.com>
2022-09-06 13:55:04 -0400

    Another form of LL-annotation

--- geocode.rb
+++ geocode.rb
@@ -1,9 +1,11 @@
 require 'google-maps'
 require 'open_street_map'
+require 'sqlite3'
 
 # A geolocator is a class that provides the 'query' method,
 # which return tuple: 'provenance', date, latitude, logitude, message
 
+# Query OpenStreetMap
 class OpenStreetMapGeolocator
   def initialize(email = nil)
     @client = OpenStreetMap::Client.new
@@ -48,6 +50,7 @@ class GoogleMapsGeolocator
   end
 end
 
+# Impose a rate limit on queries to an external provider
 class ThrottleAdaptor
   def initialize(max_queries_per_second, locator)
     @query_interval = 1 / max_queries_per_second.to_f
@@ -69,14 +72,23 @@ class ThrottleAdaptor
   end
 end
 
+# Rewrite a query w.r.t. various shorthands that appear
+# in our Richmond data feed
 class RichmondRephraser
   def initialize(locator)
     @locator = locator
   end
 
   def query(blurb)
+    q = blurb.dup
+    q.upcase!
+
     # A few entries have explicit lat/lon annotations
-    if blurb =~ /LL\((-?\d+):(\d+):(\d+\.\d+),(-?\d+):(\d+):(\d+\.\d+)\)/
+    if q =~ /LL\(ERROR!\): (.*)$/
+      # And explicit lat/lon can fail, of course.
+      q = $1.strip
+
+    elsif q =~ /LL\((-?\d+):(\d+):(\d+\.\d+),(-?\d+):(\d+):(\d+\.\d+)\)/
       lat_deg, lat_min, lat_sec = $1, $2, $3
       lon_deg, lon_min, lon_sec = $4, $5, $6
 
@@ -89,11 +101,18 @@ class RichmondRephraser
               lat_deg.to_f + lat_sign*lat_min.to_f/60 + lat_sign*lat_sec.to_f/60/60,
               lon_deg.to_f + lon_sign*lon_min.to_f/60 + lon_sign*lon_sec.to_f/60/60,
               'success']
+
+    elsif q =~ /LL\((-?\d+\.\d+),(-?\d+\.\d+)\)/
+      lat_deg = $1.to_f
+      lon_deg = $2.to_f
+
+      return ['explicit-annotation',
+              Time.now,
+              lat_deg, lon_deg,
+              'success']
     end
 
     # Try to clean up our blurb
-    q = blurb.dup
-    q.upcase!
     q.sub! /(\d+)-BLK/, '\1'
     q.sub! /:\s*ALIAS.*$/, ''
     q.sub! /\s*RICH$/, ''
@@ -106,7 +125,8 @@ class RichmondRephraser
   end
 end
 
-class CacheAdapter
+# In-memory caching of lookups
+class MemCacheAdapter
   def initialize(locator)
     @locator = locator
     @cache = {}
@@ -127,3 +147,34 @@ class CacheAdapter
     return @cache[q]
   end
 end
+
+## # Persistent cache to a sqlite file
+## class DiskCacheAdapter
+##   def initialize(locator)
+##     dbfile = File.expand_path "~/.rva-activecalls/geolocation.cache.sqlite3"
+##     @db = SQLite3::Database.new dbfile
+## 
+##     @locator = locator
+##   end
+## 
+##   def query(q)
+##     @db.execute("SELECT provenance, query_time, latitude, longitude, FROM geolocation_cache WHERE location=? LIMIT 1") each do |row|
+##       return [row[0], Time.parse(row[1]), row[2].to_f, row[3].to_f, "success" ]
+##     end
+## 
+##     begin
+##       res = @locator.query(q)
+##       @db.execute("INSERT INTO geolocation_cache (location, provenance, query_time, latitude, longitude) VALUES (?, ?, ?, ?, ?)",
+##         q, res[0], res[1], res[2], res[3])
+## 
+##     rescue SQLite3::ConstraintException => e
+##       # Rejected duplicate entries are ok
+## 
+##     rescue => e
+##       # Don't cache failure
+##       res = [nil, nil, nil, nil, nil]
+##     end
+## 
+##     return res
+##   end
+## end
--- test-geocode.rb
+++ test-geocode.rb
@@ -9,7 +9,7 @@ dbfile = File.expand_path "~/.rva-activecalls/db.sqlite3"
 db = SQLite3::Database.new dbfile
 
 geolocator = RichmondRephraser.new(
-              CacheAdapter.new(
+              MemCacheAdapter.new(
                 ThrottleAdaptor.new(10,
                   OpenStreetMapGeolocator.new(email=OPEN_STREET_MAPS_EMAIL))))
 
@@ -17,7 +17,7 @@ count_success = 0
 count_failure = 0
 
 File.open('failures.txt', 'w') do |fout|
-  db.execute("SELECT location FROM calls LIMIT 1000;").each do |row|
+  db.execute("SELECT location FROM calls ORDER BY time_received DESC LIMIT 1000;").each do |row|
     l = row[0]
     result = geolocator.query(l)
     puts "#{l} -> #{result}"
